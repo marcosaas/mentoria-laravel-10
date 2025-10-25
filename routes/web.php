ae8b4ab2f1c62adcbb524053220aeae80e9e9480
marcosaas <marcos.aas@gmail.com>
2025-10-25 18:29:13 -0300

    referencia rota no front

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
+
+});
+
+
+
+
+
