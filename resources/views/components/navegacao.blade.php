686c0efb3128c6bc36f43cadbee9f5fab181c81a
marcosaas <marcos.aas@gmail.com>
2025-10-25 16:57:57 -0300

    front-end

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,49 @@
+                        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                                        <svg class="bi" aria-hidden="true">
+                                            <use xlink:href="#house-fill"></use>
+                                        </svg>
+                                        Dashboard
+
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                        <svg class="bi" aria-hidden="true">
+                                            <use xlink:href="#file-earmark"></use>
+                                        </svg>
+                                        Vendas
+
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                        <svg class="bi" aria-hidden="true">
+                                            <use xlink:href="#cart"></use>
+                                        </svg>
+                                        Produtos
+
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                        <svg class="bi" aria-hidden="true">
+                                            <use xlink:href="#people"></use>
+                                        </svg>
+                                        Clientes
+                                    </a>
+                                </li>
+                            </ul>
+                            <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                        <svg class="bi" aria-hidden="true">
+                                            <use xlink:href="#door-closed"></use>
+                                        </svg>
+                                        SAIR
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
