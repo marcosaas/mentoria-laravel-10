126b9cb1672b73692e1206d66942ae704d4fc544
marcosaas <marcos.aas@gmail.com>
2025-10-27 15:20:27 -0300

    up

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutosController extends Controller
 {
     public function index () {
-        return 'produtos';
+
+        $findProduto = Produto::all();
+        
+        return view('pages.produtos.paginacao', compact('findProduto'));
 
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -186,12 +186,9 @@
                             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
                         </div>
                         @include('components.navegacao')
-                </div>
+                    </div>
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                     @yield('content')
-                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 class="h2">Dashboard</h1>
-                    </div>
                     {{-- <h2>Section title</h2>
                     <div class="table-responsive small">
                         <table class="table table-striped table-sm">
