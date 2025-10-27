25aabf79c1eb951e8c404e7c217148925711912d
marcosaas <marcos.aas@gmail.com>
2025-10-27 16:42:38 -0300

    up

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Produto;
+use Psy\TabCompletion\Matcher\FunctionsMatcher;
 
 class ProdutosController extends Controller
 {
-    public function index () {
+    public Function __construct(Produto $produto) 
+    {
+            $this->produto = $produto;
+    }
 
-        $findProduto = Produto::all();
-        
+    public function index (Request $request) 
+    {
+        $pesquisar = $request->pesquisar;
+        $findProduto = $this->produto->getProdutosPesquisarIndex(search: $pesquisar ?? '');
+                
         return view('pages.produtos.paginacao', compact('findProduto'));
 
     }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('/', function () {
 
 Route::prefix('produtos')->group(function () {
     Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
+    Route::delete('/delete', [ProdutosController::class, 'delete'])->name('produto.delete');
+
 
 });
 
